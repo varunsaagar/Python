931986a0627d7b597777b05de8252b7ecaa0edb7
varunsaagar <60514028+varunsaagar@users.noreply.github.com>
2020-03-25 17:50:02 +0530

    Delete Class.py

--- Assignments/Class ,Finding email/Class.py
+++
@@ -1,7 +0,0 @@
-class employee:
-        def __init__(self,rollnum,name,designation):
-            self.rollnum=rollnum
-            self.name=name
-            self.designation=designation
-
-        def
